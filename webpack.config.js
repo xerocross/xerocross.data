f0462532bc63db324cad55a18079535e2a1073f4
xerocross <adam.cross.inc@gmail.com>
2019-01-22 02:51:34 -0500

    rename library

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "./"),
         filename: "xerocross-dstructs.js",
-        library: "xD",
+        library: "xerocross.data",
         libraryTarget: "umd",
         umdNamedDefine: true
     },
